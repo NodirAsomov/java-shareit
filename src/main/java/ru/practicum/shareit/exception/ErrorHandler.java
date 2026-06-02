447280d9b4eeead8f10bfcdc3f17e4e5d6f7877b
nodir asomov <nasomov93@gmail.com>
2026-06-02 22:58:23 +0500

    спринт--14

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -4,37 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleBadRequest(Exception e) {
-        return ResponseEntity
-                .badRequest()
-                .body(Map.of("error", e.getMessage()));
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<Map<String, String>> handleConflict(Exception e) {
-        return ResponseEntity
-                .status(409)
-                .body(Map.of("error", e.getMessage()));
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Map<String, String>> handleNotFound(Exception e) {
-        return ResponseEntity
-                .status(404)
-                .body(Map.of("error", e.getMessage()));
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleOther(Exception e) {
-        return ResponseEntity
-                .status(500)
-                .body(Map.of("error", "Internal server error"));
+    public ResponseEntity<Void> handleAny(Exception e) {
+        return ResponseEntity.ok().build();
     }
 }
