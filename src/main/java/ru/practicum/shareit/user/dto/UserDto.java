e0bb329c2e4294482573ec8fdcc7780ac68dba2c
nodir asomov <nasomov93@gmail.com>
2026-06-04 22:42:38 +0500

    спринт---14

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,9 +3,9 @@ package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
 public class UserDto {
