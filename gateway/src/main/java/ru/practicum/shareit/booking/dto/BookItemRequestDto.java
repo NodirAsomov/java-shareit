f27852b6764326fe1a29790dff83d4f4c0fefd52
nodir asomov <nasomov93@gmail.com>
2026-06-25 19:36:07 +0500

    fix project

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -20,11 +18,9 @@ public class BookItemRequestDto {
 	private Long itemId;
 
 	@NotNull
-	@FutureOrPresent
 	private LocalDateTime start;
 
 	@NotNull
-	@Future
 	private LocalDateTime end;
 
 	@AssertTrue(message = "Start must be before end")
